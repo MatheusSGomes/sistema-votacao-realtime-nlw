d6626bef2560f4937eaf780f26c20d679cd6c870
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-11 19:32:18 -0300

    feat: create poll and get poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,29 +1,12 @@
 import fastity from "fastify";
-import { PrismaClient } from "@prisma/client"
-import { z } from "zod"
-
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastity();
 const HTTP_PORT = 3333;
 
-const prisma = new PrismaClient();
-
-app.post('/polls', async (request, reply) => {
-
-  const createPollBody = z.object({
-    title: z.string(),
-  })
-
-  const { title } = createPollBody.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    }
-  })
-
-  return reply.status(201).send({ pollId: poll.id })
-})
+app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: HTTP_PORT }).then(() => {
   console.log(`HTTP server running on port ${HTTP_PORT}`);
