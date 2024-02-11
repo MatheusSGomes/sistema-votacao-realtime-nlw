1d82afa1c68ebcebb426e49bca4ca2c328c2219e
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-11 20:21:55 -0300

    feat: criar inserção de votos e validação de único voto

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,22 @@
 import fastity from "fastify";
+import cookie from "@fastify/cookie"
+
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+
 
 const app = fastity();
 const HTTP_PORT = 3333;
 
+app.register(cookie, {
+  secret: "chave-secreta-polls-app",
+  hook: "onRequest",
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: HTTP_PORT }).then(() => {
   console.log(`HTTP server running on port ${HTTP_PORT}`);
