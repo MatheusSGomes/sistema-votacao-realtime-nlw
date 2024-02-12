6edba3178fce53368b4790072f6e5e8212ea9242
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-12 00:10:10 -0300

    feat: realtime com pub sub usando websocket

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -1,14 +1,19 @@
 import { FastifyInstance } from "fastify";
+import z from "zod";
+
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function pollResult(app: FastifyInstance) {
-  app.get(
-    "/polls/:pollId/results",
-    { websocket: true },
-    (connection, request) => {
-      connection.socket.on("message", (message: string) => {
-        // Se inscrever apenas nas mensagens publicadas no canal com o ID da enquete (pollId)
-        // connection.socket.send("you sent: " + message);
-      });
+  app.get( "/polls/:pollId/results", { websocket: true }, (connection, request) => {
+      const getPollParams = z.object({
+        pollId: z.string().uuid(),
+      })
+
+      const { pollId } = getPollParams.parse(request.params)
+
+      voting.subscribe(pollId, (message) => {
+        connection.socket.send(JSON.stringify(message))
+      })
     }
   );
 }
